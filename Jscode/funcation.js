7b933ca777c81c2adbde89cae4945695161fca50
Raj <209602799+imhanshhh@users.noreply.github.com>
2025-10-01 12:25:56 +0000

    Container Config done

--- Jscode/funcation.js
+++ Jscode/funcation.js
@@ -5,3 +5,18 @@ function addNumbers(a, b) {
 // How to use it:
 let sum = addNumbers(5, 10);
 console.log(sum); // Output: 15s
+
+function addAllNumbers(...numbers) {
+  let total = 0;
+  for (const num of numbers) {
+    total += num;
+  }
+  return total;
+}
+
+// How to use it:
+let sum1 = addAllNumbers(1, 2, 3);
+console.log(sum1); // Output: 6
+
+let sum2 = addAllNumbers(10, 20, 30, 40, 50);
+console.log(sum2); // Output: 150
